46e7ed43307fc720a75ae95db581ee7154bfee1d
Shami Sharma <shamisharma01@outlook.com>
2023-01-03 15:29:50 -0500

    Added an AI player, Made the battleships look more appropriate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import copy
+import random
+
 class GameBoard(object):
     def __init__(self, battleships, width, height) -> None:
         self.battleships = battleships;
@@ -48,10 +50,11 @@ class BattleShip(object):
                 el = (head[0] + i, head[1]);
 
             body.append(el)
-        return BattleShip(body);
+        return BattleShip(body, direction);
             
-    def __init__(self, body):
+    def __init__(self, body, head, length, direction):
         self.body = body;
+        self.direction = direction
         self.hits = [False] * len(body);
 
     def body_index(self, location):
@@ -62,7 +65,14 @@ class BattleShip(object):
     
     def is_destroyed(self):
         return all(self.hits)
-        
+
+
+class Player(object):
+    def __init__(self, name, shot_f):
+        self.name = name;
+        self.shot_f = shot_f;
+
+
 def renderGame(game_board, show_battleships = False):
     display = '+' + '-' * game_board.width + "+";
 
@@ -79,8 +89,27 @@ def renderGame(game_board, show_battleships = False):
      #Actually put values(Battleship) in the empty board
     if show_battleships:
         for b in game_board.battleships:
-            for x, y in b.body:
-                board[x][y] = "O";
+            for i, (x, y) in enumerate(b.body):
+                if b.direction == "N":
+                    chs = ('v', '|', 'ʌ')
+                elif b.direction == "S":
+                    chs = ('ʌ', '|', 'v')
+                elif b.direction == "W":
+                    chs = ('>', '-', '<')
+                elif b.direction == "E":
+                    chs = ('<', '-', '>')
+                else:
+                    raise "Unknown Direction"
+                
+                if i == 0:
+                    ch = chs[0];
+                elif i == len(b.body) - 1:
+                    ch = chs[2]
+                else:
+                    ch = chs[1]
+                
+                board[x][y] = ch
+
         
     for sh in game_board.shots:
         x, y = sh.location
@@ -98,20 +127,35 @@ def renderGame(game_board, show_battleships = False):
 
     print(display);
 
+def get_random_ai_shot(game_board):
+    x = random.randint(0, game_board.width - 1)
+    y = random.randint(0, game_board.height - 1)
+    return (x, y)
+
+def get_human_shot(game_board):
+     inp = input("Where do you want to shoot?\n");
+     xStr, yStr = inp.split(",");
+     x = int(xStr);
+     y = int(yStr);
+     return (x, y)
+
+    
+
 
 
 if __name__ == '__main__':
     
-    battleships = [BattleShip.build((1,1), 2, "N"),
-                   #BattleShip.build((5,8), 5, "N"),
-                   #BattleShip.build((2,3), 4, "E")
+    battleships = [BattleShip.build((1,1), 2, "S"),
+                   BattleShip.build((5,8), 5, "N"),
+                   BattleShip.build((2,3), 4, "E")
                    ]
 
     game_boards = [GameBoard(battleships, 10, 10),
                    GameBoard(copy.deepcopy(battleships), 10, 10),
                    ];
 
-    player_names = ["Frank", "AI"]
+    players = [Player('Rob', get_human_shot),
+               Player('Alice', get_random_ai_shot)]
     
     off_idx = 0;
 
@@ -119,20 +163,17 @@ if __name__ == '__main__':
         def_idx = (off_idx + 1) % 2; 
 
         defensive_board = game_boards[def_idx];
+        offensive_player = players[off_idx];
         #TODO: deal with invalid inputs 
 
-        print("%s YOUR TURN!" % player_names[off_idx])
+        print("%s YOUR TURN!" % offensive_player.name)
+        shot_location = offensive_player.shot_f(defensive_board)
         
-        inp = input("Where do you want to shoot?\n");
-        xStr, yStr = inp.split(",");
-        x = int(xStr);
-        y = int(yStr);
-
-        defensive_board.take_shot((x,y))
+        defensive_board.take_shot(shot_location)
         renderGame(defensive_board)
 
         if defensive_board.is_game_over():
-            print("%s WINS!" % player_names[off_idx]);
+            print("%s WINS!" % offensive_player.name);
             break
 
         off_idx = def_idx;
